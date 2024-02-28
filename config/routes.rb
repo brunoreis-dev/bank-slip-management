1702d81222b3d3073eca007ad7dec7ac158a1467
brunoreis-dev <b.macedoreis@icloud.com>
2024-02-28 16:06:10 -0300

    create styles css and create form html

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :bank_slips, except: [:show, :destroy]
-  # root to: 'bank_slips#index'
+  root to: 'bank_slips#index'
   patch "bank_slips/cancel/:id", to: "bank_slips#cancel", as: :bank_slips_cancel
 
   # Defines the root path route ("/")
